c04b6a9d256df4395e67c2542e188beb2b41b523
Sier Santos <louisasantosl12@gmail.com>
2022-06-01 11:58:20 +0900

    Uploaded program

---
+++ Tic Tac Toe Program/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace Tic_Tac_Toe_Program
+{
+    partial class TicTacToe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.MenuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuFileNewG = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuFileExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuHelp = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuHelpProcedure = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuCreatorAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuCreatorGame = new System.Windows.Forms.ToolStripMenuItem();
+            this.TicTacbtn1 = new System.Windows.Forms.Button();
+            this.TicTacbtn2 = new System.Windows.Forms.Button();
+            this.TicTacbtn3 = new System.Windows.Forms.Button();
+            this.TicTacbtn4 = new System.Windows.Forms.Button();
+            this.TicTacbtn5 = new System.Windows.Forms.Button();
+            this.TicTacbtn6 = new System.Windows.Forms.Button();
+            this.TicTacbtn7 = new System.Windows.Forms.Button();
+            this.TicTacbtn8 = new System.Windows.Forms.Button();
+            this.TicTacbtn9 = new System.Windows.Forms.Button();
+            this.btnreset = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuFile,
+            this.MenuHelp,
+            this.MenuAbout});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(362, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // MenuFile
+            // 
+            this.MenuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuFileNewG,
+            this.MenuFileExit});
+            this.MenuFile.Name = "MenuFile";
+            this.MenuFile.Size = new System.Drawing.Size(37, 20);
+            this.MenuFile.Text = "File";
+            // 
+            // MenuFileNewG
+            // 
+            this.MenuFileNewG.Name = "MenuFileNewG";
+            this.MenuFileNewG.Size = new System.Drawing.Size(152, 22);
+            this.MenuFileNewG.Text = "New Game";
+            this.MenuFileNewG.Click += new System.EventHandler(this.MenuFileNewG_Click);
+            // 
+            // MenuFileExit
+            // 
+            this.MenuFileExit.Name = "MenuFileExit";
+            this.MenuFileExit.Size = new System.Drawing.Size(152, 22);
+            this.MenuFileExit.Text = "Exit";
+            this.MenuFileExit.Click += new System.EventHandler(this.MenuFileExit_Click);
+            // 
+            // MenuHelp
+            // 
+            this.MenuHelp.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuHelpProcedure});
+            this.MenuHelp.Name = "MenuHelp";
+            this.MenuHelp.Size = new System.Drawing.Size(44, 20);
+            this.MenuHelp.Text = "Help";
+            // 
+            // MenuHelpProcedure
+            // 
+            this.MenuHelpProcedure.Name = "MenuHelpProcedure";
+            this.MenuHelpProcedure.Size = new System.Drawing.Size(128, 22);
+            this.MenuHelpProcedure.Text = "Procedure";
+            this.MenuHelpProcedure.Click += new System.EventHandler(this.MenuHelpProcedure_Click);
+            // 
+            // MenuAbout
+            // 
+            this.MenuAbout.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuCreatorAbout,
+            this.MenuCreatorGame});
+            this.MenuAbout.Name = "MenuAbout";
+            this.MenuAbout.Size = new System.Drawing.Size(52, 20);
+            this.MenuAbout.Text = "About";
+            // 
+            // MenuCreatorAbout
+            // 
+            this.MenuCreatorAbout.Name = "MenuCreatorAbout";
+            this.MenuCreatorAbout.Size = new System.Drawing.Size(113, 22);
+            this.MenuCreatorAbout.Text = "Creator";
+            this.MenuCreatorAbout.Click += new System.EventHandler(this.MenuCreatorAbout_Click);
+            // 
+            // MenuCreatorGame
+            // 
+            this.MenuCreatorGame.Name = "MenuCreatorGame";
+            this.MenuCreatorGame.Size = new System.Drawing.Size(113, 22);
+            this.MenuCreatorGame.Text = "Game";
+            this.MenuCreatorGame.Click += new System.EventHandler(this.MenuCreatorGame_Click);
+            // 
+            // TicTacbtn1
+            // 
+            this.TicTacbtn1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TicTacbtn1.Location = new System.Drawing.Point(13, 28);
+            this.TicTacbtn1.Name = "TicTacbtn1";
+            this.TicTacbtn1.Size = new System.Drawing.Size(102, 91);
+            this.TicTacbtn1.TabIndex = 1;
+            this.TicTacbtn1.Text = " ";
+            this.TicTacbtn1.UseVisualStyleBackColor = true;
+            this.TicTacbtn1.Click += new System.EventHandler(this.btnclick);
+            // 
+            // TicTacbtn2
+            // 
+            this.TicTacbtn2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TicTacbtn2.Location = new System.Drawing.Point(129, 28);
+            this.TicTacbtn2.Name = "TicTacbtn2";
+            this.TicTacbtn2.Size = new System.Drawing.Size(102, 91);
+            this.TicTacbtn2.TabIndex = 2;
+            this.TicTacbtn2.UseVisualStyleBackColor = true;
+            this.TicTacbtn2.Click += new System.EventHandler(this.btnclick);
+            // 
+            // TicTacbtn3
+            // 
+            this.TicTacbtn3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TicTacbtn3.Location = new System.Drawing.Point(246, 28);
+            this.TicTacbtn3.Name = "TicTacbtn3";
+            this.TicTacbtn3.Size = new System.Drawing.Size(102, 91);
+            this.TicTacbtn3.TabIndex = 3;
+            this.TicTacbtn3.UseVisualStyleBackColor = true;
+            this.TicTacbtn3.Click += new System.EventHandler(this.btnclick);
+            // 
+            // TicTacbtn4
+            // 
+            this.TicTacbtn4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TicTacbtn4.Location = new System.Drawing.Point(13, 125);
+            this.TicTacbtn4.Name = "TicTacbtn4";
+            this.TicTacbtn4.Size = new System.Drawing.Size(102, 91);
+            this.TicTacbtn4.TabIndex = 6;
+            this.TicTacbtn4.UseVisualStyleBackColor = true;
+            this.TicTacbtn4.Click += new System.EventHandler(this.btnclick);
+            // 
+            // TicTacbtn5
+            // 
+            this.TicTacbtn5.Font = new System.Drawing.Font("Arial Rounded MT Bold", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TicTacbtn5.Location = new System.Drawing.Point(129, 125);
+            this.TicTacbtn5.Name = "TicTacbtn5";
+            this.TicTacbtn5.Size = new System.Drawing.Size(102, 91);
+            this.TicTacbtn5.TabIndex = 5;
+            this.TicTacbtn5.UseVisualStyleBackColor = true;
+            this.TicTacbtn5.Click += new System.EventHandler(this.btnclick);
+            // 
+            // TicTacbtn6
+            // 
+            this.TicTacbtn6.Font = new System.Drawing.Font("Arial Rounded MT Bold", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TicTacbtn6.Location = new System.Drawing.Point(246, 125);
+            this.TicTacbtn6.Name = "TicTacbtn6";
+            this.TicTacbtn6.Size = new System.Drawing.Size(102, 91);
+            this.TicTacbtn6.TabIndex = 4;
+            this.TicTacbtn6.UseVisualStyleBackColor = true;
+            this.TicTacbtn6.Click += new System.EventHandler(this.btnclick);
+            // 
+            // TicTacbtn7
+            // 
+            this.TicTacbtn7.Font = new System.Drawing.Font("Arial Rounded MT Bold", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TicTacbtn7.Location = new System.Drawing.Point(13, 222);
+            this.TicTacbtn7.Name = "TicTacbtn7";
+            this.TicTacbtn7.Size = new System.Drawing.Size(102, 91);
+            this.TicTacbtn7.TabIndex = 9;
+            this.TicTacbtn7.UseVisualStyleBackColor = true;
+            this.TicTacbtn7.Click += new System.EventHandler(this.btnclick);
+            // 
+            // TicTacbtn8
+            // 
+            this.TicTacbtn8.Font = new System.Drawing.Font("Arial Rounded MT Bold", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TicTacbtn8.Location = new System.Drawing.Point(129, 222);
+            this.TicTacbtn8.Name = "TicTacbtn8";
+            this.TicTacbtn8.Size = new System.Drawing.Size(102, 91);
+            this.TicTacbtn8.TabIndex = 8;
+            this.TicTacbtn8.UseVisualStyleBackColor = true;
+            this.TicTacbtn8.Click += new System.EventHandler(this.btnclick);
+            // 
+            // TicTacbtn9
+            // 
+            this.TicTacbtn9.Font = new System.Drawing.Font("Arial Rounded MT Bold", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TicTacbtn9.Location = new System.Drawing.Point(246, 222);
+            this.TicTacbtn9.Name = "TicTacbtn9";
+            this.TicTacbtn9.Size = new System.Drawing.Size(102, 91);
+            this.TicTacbtn9.TabIndex = 7;
+            this.TicTacbtn9.UseVisualStyleBackColor = true;
+            this.TicTacbtn9.Click += new System.EventHandler(this.btnclick);
+            // 
+            // btnreset
+            // 
+            this.btnreset.Font = new System.Drawing.Font("Britannic Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnreset.Location = new System.Drawing.Point(118, 319);
+            this.btnreset.Name = "btnreset";
+            this.btnreset.Size = new System.Drawing.Size(122, 26);
+            this.btnreset.TabIndex = 10;
+            this.btnreset.Text = "RESET";
+            this.btnreset.UseVisualStyleBackColor = true;
+            this.btnreset.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // TicTacToe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(362, 351);
+            this.Controls.Add(this.btnreset);
+            this.Controls.Add(this.TicTacbtn7);
+            this.Controls.Add(this.TicTacbtn8);
+            this.Controls.Add(this.TicTacbtn9);
+            this.Controls.Add(this.TicTacbtn4);
+            this.Controls.Add(this.TicTacbtn5);
+            this.Controls.Add(this.TicTacbtn6);
+            this.Controls.Add(this.TicTacbtn3);
+            this.Controls.Add(this.TicTacbtn2);
+            this.Controls.Add(this.TicTacbtn1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "TicTacToe";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tic Tac Toe";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem MenuFile;
+        private System.Windows.Forms.ToolStripMenuItem MenuHelp;
+        private System.Windows.Forms.ToolStripMenuItem MenuAbout;
+        private System.Windows.Forms.ToolStripMenuItem MenuFileNewG;
+        private System.Windows.Forms.ToolStripMenuItem MenuFileExit;
+        private System.Windows.Forms.ToolStripMenuItem MenuHelpProcedure;
+        private System.Windows.Forms.ToolStripMenuItem MenuCreatorAbout;
+        private System.Windows.Forms.ToolStripMenuItem MenuCreatorGame;
+        private System.Windows.Forms.Button TicTacbtn1;
+        private System.Windows.Forms.Button TicTacbtn2;
+        private System.Windows.Forms.Button TicTacbtn3;
+        private System.Windows.Forms.Button TicTacbtn4;
+        private System.Windows.Forms.Button TicTacbtn5;
+        private System.Windows.Forms.Button TicTacbtn6;
+        private System.Windows.Forms.Button TicTacbtn7;
+        private System.Windows.Forms.Button TicTacbtn8;
+        private System.Windows.Forms.Button TicTacbtn9;
+        private System.Windows.Forms.Button btnreset;
+    }
+}
+
---
+++ Tic Tac Toe Program/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe_Program
+{
+    public partial class TicTacToe : Form
+    {
+        bool PlayerTurn = true;
+        int PlayerTurn_count = 0;
+        
+
+        public TicTacToe()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuCreatorAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello Player!\r\n" + "\r\n" + "The program was created by a BSCOE Student\r\n" + "at PUP named Louisa Reighn A. Santos.", "About the Creator");
+        }
+
+        private void MenuCreatorGame_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("   Tic-Tac-Toe is a game in which two players seek\r\n" + "in alternate turns to complete a row, a column, or \r\n" + "a diagonal with either three O's or three X's drawn \r\n" + "in the spaces of a grid of nine squares.", "About the Game");
+        }
+
+        private void MenuHelpProcedure_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Steps in playing Tic-Tac-Toe:\r\n" + "1. Have the first player go first.\r\n" + "2. Have the second player go next.\r\n" + "3. Keep alternating moves until one has drawn a row of three X or O.\r\n" + "4. Enjoy the game!", "How to play Tic-Tac-Toe");
+        }
+
+        private void MenuFileExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnclick(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (PlayerTurn)
+            {
+                btn.Text = "X";
+                btn.BackColor = Color.PaleGoldenrod;
+            }
+            else
+            {
+                btn.Text = "O";
+                btn.BackColor = Color.LightGoldenrodYellow;
+            }
+
+            PlayerTurn = !PlayerTurn;   //to create the alternating turn
+            btn.Enabled = false;        //to disable the button after clicking once
+            PlayerTurn_count++;
+
+            checking_of_winner();
+        }
+
+        private void checking_of_winner()
+        {
+            bool TheWinner = false;
+
+            //Horizontal
+            if ((TicTacbtn1.Text == TicTacbtn2.Text) && (TicTacbtn2.Text == TicTacbtn3.Text) && (!TicTacbtn1.Enabled))
+                TheWinner = true;
+            else if ((TicTacbtn4.Text == TicTacbtn5.Text) && (TicTacbtn5.Text == TicTacbtn6.Text) && (!TicTacbtn4.Enabled))
+                TheWinner = true;
+            else if ((TicTacbtn7.Text == TicTacbtn8.Text) && (TicTacbtn8.Text == TicTacbtn9.Text) && (!TicTacbtn7.Enabled))
+                TheWinner = true;
+
+            //Vertical
+            else if ((TicTacbtn1.Text == TicTacbtn4.Text) && (TicTacbtn4.Text == TicTacbtn7.Text) && (!TicTacbtn1.Enabled))
+                TheWinner = true;
+            else if ((TicTacbtn2.Text == TicTacbtn5.Text) && (TicTacbtn5.Text == TicTacbtn8.Text) && (!TicTacbtn2.Enabled))
+                TheWinner = true;
+            else if ((TicTacbtn3.Text == TicTacbtn6.Text) && (TicTacbtn6.Text == TicTacbtn9.Text) && (!TicTacbtn3.Enabled))
+                TheWinner = true;
+
+            //Diagonal
+            else if ((TicTacbtn1.Text == TicTacbtn5.Text) && (TicTacbtn5.Text == TicTacbtn9.Text) && (!TicTacbtn1.Enabled))
+                TheWinner = true;
+            else if ((TicTacbtn3.Text == TicTacbtn5.Text) && (TicTacbtn5.Text == TicTacbtn7.Text) && (!TicTacbtn5.Enabled))
+                TheWinner = true;
+
+
+            if (TheWinner)
+            {
+                disable_Buttons();
+
+                string champion = "";
+                if (PlayerTurn)
+                    champion = "O";
+                else 
+                    champion = "X";
+                
+                MessageBox.Show("The Winner is" + " " + champion + "!\n" + "Select New Game to play again.", "TicTacToe Winner!");
+            }
+            else
+            {
+                if (PlayerTurn_count == 9)
+                    MessageBox.Show("Draw!" + " " + "Please restart the game.", "Yikes!");
+            }
+
+        }
+
+        private void disable_Buttons()
+        {
+            try
+            {
+                foreach (Control dis in Controls)
+                {
+                    Button btn = (Button)dis;
+                    btn.Enabled = false;
+                }
+            }
+            catch { }
+        }
+
+        private void MenuFileNewG_Click(object sender, EventArgs e)
+        {
+            PlayerTurn = true;
+            PlayerTurn_count = 0;
+            try
+            {
+                foreach (Control dis in Controls)
+                {
+                    Button btn = (Button)dis;
+                    btn.Enabled = true;
+                    btn.Text = "";
+                    btn.BackColor = System.Drawing.Color.White;
+                    btnreset.Text = "RESET";
+                }
+            }
+            catch { }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            PlayerTurn = true;
+            PlayerTurn_count = 0;
+            try
+            {
+                foreach (Control dis in Controls)
+                {
+                    Button btn = (Button)dis;
+                    btn.Enabled = true;
+                    btn.Text = "";
+                    btn.BackColor = System.Drawing.Color.White;
+                    btnreset.Text = "RESET";
+                }
+                
+            }
+            catch { }
+        }
+  
+    }
+}
+
